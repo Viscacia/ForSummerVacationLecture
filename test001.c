e1d1c59b362915e9c59914394ebff73acf021f87
LAPTOP-E67QPAOJ\ksmms <ksmmsk0@gmail.com>
2021-07-19 15:14:47 +0900

    after revert

--- test001.c
+++ test001.c
@@ -10,6 +10,17 @@ int main() {
 	printf("%d", c);
 	printf("edited_2");
 	printf("edited_3");
+<<<<<<< HEAD
+	printf("edited_4_1");
+=======
+<<<<<<< HEAD
 	printf("edited_4");
+<<<<<<< HEAD
+=======
+=======
+	printf("edited_4_1");
+>>>>>>> update_branch
+>>>>>>> main
+>>>>>>> parent of 29141cb (arragement)
 	return 0;
 }
